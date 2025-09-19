3a508c4b266ae0857ce3403f3ffacc081cdb2a67
ThoND <thonguyenduc2010@gmail.com>
2025-09-19 16:04:00 +0700

    init

---
+++ addons/point_of_sale/static/src/app/models/pos_config.js
@@ -0,0 +1,57 @@
+import { registry } from "@web/core/registry";
+import { Base } from "./related_models";
+
+export class PosConfig extends Base {
+    static pythonModel = "pos.config";
+
+    initState() {
+        super.initState();
+        this.uiState = {};
+    }
+
+    get canInvoice() {
+        return Boolean(this.raw.invoice_journal_id);
+    }
+
+    get useProxy() {
+        return (
+            this.is_posbox &&
+            (this.iface_electronic_scale ||
+                this.iface_print_via_proxy ||
+                this.iface_scan_via_proxy ||
+                this.iface_customer_facing_display_via_proxy)
+        );
+    }
+
+    get isShareable() {
+        return this.raw.trusted_config_ids.length > 0;
+    }
+
+    get printerCategories() {
+        const set = new Set();
+        for (const relPrinter of this.models["pos.printer"].getAll()) {
+            const printer = relPrinter.raw;
+            for (const id of printer.product_categories_ids) {
+                set.add(id);
+            }
+        }
+        return set;
+    }
+
+    get preparationCategories() {
+        if (this.printerCategories) {
+            return new Set([...this.printerCategories]);
+        }
+        return new Set();
+    }
+
+    get displayBigTrackingNumber() {
+        return false;
+    }
+
+    get displayTrackingNumber() {
+        return this.module_pos_restaurant;
+    }
+}
+
+registry.category("pos_available_models").add(PosConfig.pythonModel, PosConfig);
